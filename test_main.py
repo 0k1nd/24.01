944e0044c949cbc8ecb5fb46baee87c9def8fe8f
0k1nd <akindtyrbo@gmail.com>
2025-01-24 13:04:27 +0300

    commit before reload pk

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 from fastapi.testclient import TestClient
+import os
 
 from .main import app
 
@@ -7,4 +8,22 @@ client = TestClient(app)
 
 
 def test_get_calltouch():
-    response = client.get()
+    siteId = os.getenv("SITE_ID")
+    clientApiId = os.getenv("CLIENTAPILD")
+    dateFrom = '29/11/2024'
+    dateTo = '29/11/2024'
+    page = 3
+    limit = 2
+    response = client.get("/calltouch/", params={
+        "siteId": siteId,
+        "clientApiId": clientApiId,
+        "dateFrom": dateFrom,
+        "dateTo": dateTo,
+        "page": page,
+        "limit": limit
+    })
+    assert response.status_code == 200
+    data = response.json()
+    print(data)
+    assert isinstance(data, list)
+    assert "callId" in data
